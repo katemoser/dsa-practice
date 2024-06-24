d3a55715baa52bdd6f92d3337d48d4933886d0e8
Kate Moser <komoser@gmail.com>
2024-06-24 10:08:28 -0700

    warmup 6/24

---
+++ codewars/5-rgb-to-hex.js
@@ -0,0 +1,31 @@
+/**
+ *  The rgb function is incomplete. Complete it so that passing in RGB decimal
+ * values will result in a hexadecimal representation being returned. Valid
+ * decimal values for RGB are 0 - 255. Any values that fall out of that range
+ * must be rounded to the closest valid value.
+ *
+ * Note: Your answer should always be 6 characters long, the shorthand with 3
+ * will not work here.
+
+  Examples (input --> output):
+  255, 255, 255 --> "FFFFFF"
+  255, 255, 300 --> "FFFFFF"
+  0, 0, 0       --> "000000"
+  148, 0, 211   --> "9400D3"
+ *
+ */
+
+function rgb(...colors) {
+  return colors.map(c => toHex(c)).join("")
+}
+
+function toHex(num){
+  if (num < 0) return "00";
+
+  if( num > 255) return "FF";
+  let hex = num.toString(16);
+  if( hex.length <= 1){
+    hex = "0" + hex;
+  }
+  return hex.toUpperCase()
+}
